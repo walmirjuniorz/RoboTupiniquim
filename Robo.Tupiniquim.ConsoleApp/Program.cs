ef19fa5e1951f799b81d305a9d8fd23ceb3be268
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-28 22:05:34 -0300

    Implementação inicial do aplicativo de console Robo Tupiniquim

--- Robo.Tupiniquim.ConsoleApp/Program.cs
+++ Robo.Tupiniquim.ConsoleApp/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("Robô Tupiniquim");
+        Console.WriteLine("-------------------------");
+
+        Console.Write("Digite a posição inicial (X,Y): ");
+        string posicaoInicial = Console.ReadLine();
+
+        string[] posicao = new string[3];
+        string valorAtual = "";
+        int indice = 0;
+
+        for (int i = 0; i < posicaoInicial.Length; i++)
+        {
+            if (posicaoInicial[i] != ' ')
+            {
+                valorAtual = valorAtual + posicaoInicial[i];
+            }
+            else
+            {
+                posicao[indice] = valorAtual;
+                valorAtual = "";
+                indice++;
+            }
+        }
+        posicao[indice] = valorAtual;
+
+        Console.Clear();
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("Lista de Comandos");
+        Console.WriteLine("E (Esquerda)");
+        Console.WriteLine("D (Direita)");
+        Console.WriteLine("M (Mover)");
+        Console.WriteLine("-------------------------");
+        Console.Write("Digite o Comando: ");
+        string comando = Console.ReadLine();
+
+        char[] instrucoes = comando.ToCharArray();
+
+        for (int i = 0; i < comando.Length; i++)
+        {
+            char comandoAtual = comando[i];
+
+            if (comandoAtual == 'E')
+            {
+
+            }
+            else if (comandoAtual == 'D')
+            {
+
+            }
+            else if (comandoAtual == 'M')
+            {
+
+            }
+        }
+
+
+    }
+}
+
+
